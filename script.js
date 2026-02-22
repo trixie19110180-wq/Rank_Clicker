a8805a6305d3add674bb02f4bb7fc35c97253d8d
TrixieDaJavascriptCoder <trixie19110180@gmail.com>
2026-02-22 19:56:10 +0900

    Add upgrade functionality to points system

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let points = 0
+let upgradeAmt = 1
+let upgradeView = 1
 const pointsDisplay = document.getElementById("points")
 const clickButton = document.getElementById("clickButton")
+const upgradeClick = document.getElementById("upgradeClick")
 clickButton.addEventListener("click", function() {
-  points++
+  points += upgradeAmt
   pointsDisplay.innerText = points
+})
+
+upgradeClick.addEventListener("click", function() {
+  upgradeAmt *= 1.2
+  upgradeView = Math.ceil(upgradeAmt)
 })
